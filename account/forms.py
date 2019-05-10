158f1aa7957c64a94a84a02608bd6bb41b5bb524
sujunfeng <76686094@qq.com>
2019-05-10 18:35:50 +0800

    登录和退出，注册页面更新

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserProfile
+# forms.Form 无sql操作 forms.ModelForm 有数据库操作
 
 
 class LoginForm(forms.Form):
@@ -20,3 +22,9 @@ class RegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("Password don't match.")
         return cd['password']
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ("phone", "birth")
